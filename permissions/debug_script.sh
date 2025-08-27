9fd7a8f61913f5b71312238894975fb24c163128
akoll927 <11927@holbertonstudents.com>
2025-08-27 09:47:17 +0000

    add script 16-if_only

--- permissions/debug_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Current owner: $(stat -c "%U" hello 2>/dev/null)"
-if [ "$(stat -c "%U" hello 2>/dev/null)" = "guillaume" ]; then
-    echo "Changing to vincent..."
-    chown vincent hello
-else
-    echo "Not changing - owner is not guillaume"
-fi
